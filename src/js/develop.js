7405eab9ccce5fa734467bcde058337aa29fa53e
trolizmaslom <trolizmaslom@gmail.com>
2017-07-13 17:25:03 +0300

    rr

---
+++ src/js/develop.js
@@ -0,0 +1,87 @@
+function selectStyler(){
+   var item = $('.js-select-lang');
+   if(item.length > 0){
+       item.styler({
+           selectSmartPositioning:false
+       });
+   }
+}
+function headerSearch() {
+    var container = $('.js-search');
+    if(!container.length)return;
+    var input = container.find('input').eq(0);
+    var list = container.find('.header__result').eq(0);
+    input.focus(function(){
+        list.stop().slideDown(200);
+    });
+    input.blur(function(){
+        var a = false;
+        list[0].addEventListener('mousemove', function(e) {
+            if(document.elementFromPoint(e.clientX, e.clientY).tagName == 'A'){a=true;}
+        }, false);
+        if(!a){list.stop().slideUp();}
+    });
+}
+function dropdown() {
+    $(document).on('click', '.js-dropdown-button', function () {
+        var btn = $(this);
+        var container = btn.closest('.js-dropdown-container');
+        var block = container.find('.js-dropdown-block').eq(0);
+        btn.toggleClass('active');
+        if(btn.hasClass('active')){
+            block.stop().slideDown(200);
+        }else{
+            block.stop().slideUp(200);
+        }
+        $(document).on('click touchstart',function (event){
+            var ignore = $('.js-ignore-miss-click');
+            if (!btn.is(event.target) && btn.has(event.target).length === 0 && !container.is(event.target) && container.has(event.target).length === 0 && !ignore.is(event.target) && ignore.has(event.target).length === 0){
+                btn.removeClass('active');
+                block.stop().slideUp(200);
+            }
+        });
+    })
+}
+function slidersInit() {
+    var slider = $('.js-fade-slider');
+    if(slider.length > 0){
+        slider.slick({
+            dots: false,
+            arrows: false,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear'
+        });
+        $(document).on('click', '.js-fade-next', function () {
+            slider.slick('slickNext');
+        });
+        $(document).on('click', '.js-fade-prev', function () {
+            slider.slick('slickPrev');
+        });
+    }
+}
+function startTime(clock) {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    m = checkTime(m);
+    clock.text(h + ":" + m);
+}
+function checkTime(i) {
+    if (i < 10) {i = "0" + i};
+    return i;
+}
+function clock() {
+    var clock = $('.js-clock');
+    if(clock.length > 0){ startTime(clock);}
+}
+$(document).ready(function () {
+    setTimeout(function () {
+        selectStyler();
+    },100);
+    headerSearch();
+    dropdown();
+    slidersInit();
+    clock();
+});
